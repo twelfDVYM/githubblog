2165218ad9e1d3ccd51831a075c7d19b144838ef
Daniel YM <twelfD@outlook.com>
2025-04-02 10:41:02 +0200

    wildcard

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "Vee9168.github.io",
+        hostname: "**.github.io",
         pathname: "/**",
       },
       {
